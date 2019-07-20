f57efc09054f4e405b3d1cd284a1de1c572f3512
vagrant <jin541223@163.com>
2019-07-20 15:45:43 +0000

    基本页面及用户认证等

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="http:://github.com/jin541223" target="_blank">zera</a> 设计编码
+      <span style="color: #e27575;font-size: 14px;">❥(^_-)</span>
+    </p>
+
+    <p class="float-right">
+      <a href="mailto:474638401@qq.com">联系我们</a>
+  </div>
+</footer>
