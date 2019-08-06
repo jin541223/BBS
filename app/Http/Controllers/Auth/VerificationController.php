ba5f2db168a8f77a0211873d88f22ff3e87ef503
vagrant <jin541223@163.com>
2019-08-06 15:00:07 +0000

    邮箱验证后消息通知~

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -34,8 +34,12 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        // 所有控制器内的方法都必须登录后才能访问
         $this->middleware('auth');
+        // 设定只有verify动作使用signed中间件进行认证；signed是URL前面认证方式
         $this->middleware('signed')->only('verify');
+        // 对verify、resend动作做频率限制
+        // throttle中间件是框架提供的访问频率限制功能 如此处表示一分钟不能超过六次
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
